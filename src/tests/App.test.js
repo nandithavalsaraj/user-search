88f251083ab4c7d24a4c21f69db5405a8677daa4
Nanditha Valsaraj <40928820+nandithavalsaraj@users.noreply.github.com>
2020-09-12 15:51:32 +0530

    Test component rendering
    - Test to check if components renders without crashing

---
+++ src/tests/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { render } from '@testing-library/react'
+import App from '../components/App'
+import { isTSAnyKeyword } from '@babel/types'
+
+test('renders learn react link', () => {
+    const { getByText } = render(<App />)
+    const linkElement = getByText(/learn react/i)
+    expect(linkElement).toBeInTheDocument()
+})
