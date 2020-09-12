0d843baa63a47384c811f46ae6d1e23f486497c9
Nanditha Valsaraj <40928820+nandithavalsaraj@users.noreply.github.com>
2020-09-12 15:53:00 +0530

    Test  component rendering

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,8 +4,7 @@ import { render } from '@testing-library/react'
 import App from '../components/App'
 import { isTSAnyKeyword } from '@babel/types'
 
-test('renders learn react link', () => {
-    const { getByText } = render(<App />)
-    const linkElement = getByText(/learn react/i)
-    expect(linkElement).toBeInTheDocument()
+test('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
 })
